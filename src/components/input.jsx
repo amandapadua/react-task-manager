7148339b6ffa486dd02c3be9583a0a75b742ba41
devpadua <amanda.p.transporteelogistica@gmail.com>
2026-06-02 20:53:12 -0300

    fix: corrigir Input.jsx

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,12 +1,13 @@
-function Input(props){
-return 
- <input
-  className="w-full p-2 rounded border outline-slate-400" 
-  type= {props, type}
-  value={props.value}
-  placeholder= {props.placeholder}
-  onchange = {props.onChange}/>
-
+function Input(props) {
+  return (
+    <input
+      className="w-full p-2 rounded border outline-slate-400"
+      type={props.type}
+      value={props.value}
+      placeholder={props.placeholder}
+      onChange={props.onChange}
+    />
+  )
 }
 
 export default Input;
